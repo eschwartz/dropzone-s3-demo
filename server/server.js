9c194e0d652ea86074a1b6fcf1ee114251a5512b
Edan Schwartz <edanschwartz@gmail.com>
2020-08-03 17:23:05 -0500

    Working with createPresignedPost

--- server/server.js
+++ server/server.js
@@ -10,25 +10,22 @@ app.post('/upload-url', async (req, res) => {
   let expires = 900;
   console.log('yo');
 
+  s3.createPresignedPost( {
+    Bucket: 'nagle-demo-for-sam',
+    Expires: expires,
+    Fields: {
+      key: 'test-file.txt',
+    }
+  }, (err, data) => {
+    if (err) {
+      console.error(err);
+      res.send(500);
+      return;
+    }
 
-  let url;
-  try {
-    url = await s3.getSignedUrlPromise("putObject", {
-      Bucket: 'nagle-demo-for-sam',
-      Key: 'test-file',
-      Expires: expires
-    });
-  }
-  catch (err) {
-    console.error(err);
-    res.sendStatus(500);
-    return;
-  }
+    res.send(data);
+  })
 
-  res.send({
-    expires,
-    url
-  });
 });
 
 app.listen(PORT, () => {
--- src/App.js
+++ src/App.js
@@ -10,9 +10,29 @@ function App() {
     const res = await axios.post('/upload-url');
     console.log("POST /upload-url", res.data);
 
-    return { 
+    const params = { 
+      fields: res.data.fields, 
+      meta: { 
+        fileUrl: `${res.data.url}/${res.data.fields.key}`
+      }, 
       url: res.data.url 
     };
+    console.log('params', params);
+
+    // ENABLE CORS on your bucket
+    /**
+      <?xml version="1.0" encoding="UTF-8"?>
+      <CORSConfiguration xmlns="http://s3.amazonaws.com/doc/2006-03-01/">
+      <CORSRule>
+          <AllowedOrigin>http://localhost:3000</AllowedOrigin>
+          <AllowedMethod>POST</AllowedMethod>
+          <AllowedHeader>*</AllowedHeader>
+      </CORSRule>
+      </CORSConfiguration>
+
+     */
+
+    return params;
   }
   
   // receives array of files that are done uploading when submit button is clicked
