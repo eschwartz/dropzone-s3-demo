5373b15977e34b07fff6ebe01a2d8448c110cba7
Edan Schwartz <edanschwartz@gmail.com>
2020-08-04 09:52:21 -0500

    Extract S3Uploader component

--- src/App.js
+++ src/App.js
@@ -1,55 +1,14 @@
 import React from 'react';
 import './App.css';
 import 'react-dropzone-uploader/dist/styles.css';
-import Dropzone from 'react-dropzone-uploader';
-import axios from 'axios';
+import S3Uploader from './S3Uploader';
 
 function App() {
-  // specify upload params and url for your files
-  const getUploadParams = async ({ meta }) => { 
-    const res = await axios.post('/upload-url');
-    console.log("POST /upload-url", res.data);
-
-    const params = { 
-      fields: res.data.fields, 
-      meta: { 
-        fileUrl: `${res.data.url}/${res.data.fields.key}`
-      }, 
-      url: res.data.url 
-    };
-    console.log('params', params);
-
-    // ENABLE CORS on your bucket
-    /**
-      <?xml version="1.0" encoding="UTF-8"?>
-      <CORSConfiguration xmlns="http://s3.amazonaws.com/doc/2006-03-01/">
-      <CORSRule>
-          <AllowedOrigin>http://localhost:3000</AllowedOrigin>
-          <AllowedMethod>POST</AllowedMethod>
-          <AllowedHeader>*</AllowedHeader>
-      </CORSRule>
-      </CORSConfiguration>
-
-     */
-
-    return params;
-  }
-  
-  // receives array of files that are done uploading when submit button is clicked
-  const handleSubmit = (files, allFiles) => {
-    console.log(files.map(f => f.meta))
-    allFiles.forEach(f => f.remove())
-  }
 
   return (
     <>
       <div className="app">
-
-        <h1>Upload Files!</h1>
-        <Dropzone
-          getUploadParams={getUploadParams}
-          onSubmit={handleSubmit}
-        />
+        <S3Uploader />
       </div>
     </>
   )
